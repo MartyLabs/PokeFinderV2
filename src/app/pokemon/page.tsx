143d41003a177115e1d7802f81fa490188c264cb
arthur.mariet@epitech.eu <arthur.mariet@epitech.eu>
2025-03-20 20:05:01 +0100

    feat: add client-side Pokémon filtering by type with UI highlight

--- src/app/pokemon/page.tsx
+++ src/app/pokemon/page.tsx
@@ -8,6 +8,7 @@ import PokeDetails from "@/components/Pokemon/PokeDetails/PokeDetails";
 import Spinner from "@/components/UI/Spinner";
 import ReloadButton from "@/components/UI/ReloadButton";
 import PageToggle from "@/components/UI/PageToggle";
+import FilterButton from "@/components/UI/FilterButton";
 
 const GET_POKEMONS = gql`
   query {
@@ -27,9 +28,13 @@ const GET_POKEMONS = gql`
 `;
 
 export default function PokemonPage() {
-  const { loading, error, data } = useQuery(GET_POKEMONS);
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [currentPokemon, setCurrentPokemon] = useState();
+  const [selectedType, setSelectedType] = useState<string | null>(null);
+
+  const { loading, error, data } = useQuery(GET_POKEMONS, {
+    variables: { limit: 10 },
+  });
 
   if (!data || !data.pokemon_v2_pokemon) {
     return (
@@ -37,9 +42,18 @@ export default function PokemonPage() {
     );
   }
 
-  const filteredPokemons = data.pokemon_v2_pokemon.filter((pokemon: any) =>
-    pokemon.name.toLowerCase().startsWith(searchTerm.toLowerCase())
-  );
+  const filteredPokemons = data.pokemon_v2_pokemon
+    .filter(
+      (pokemon: any) =>
+        pokemon.name.toLowerCase().startsWith(searchTerm.toLowerCase()) // Filtrage par nom
+    )
+    .filter((pokemon: any) =>
+      selectedType
+        ? pokemon.pokemon_v2_pokemontypes.some(
+            (type: any) => type.pokemon_v2_type.name === selectedType
+          )
+        : true
+    );
 
   if (loading) return <Spinner />;
   if (error) return <p>Error: {error.message}</p>;
@@ -50,6 +64,11 @@ export default function PokemonPage() {
         <div className="flex flex-row justify-between items-center space-x-4">
           <SearchBar onChange={setSearchTerm} />
           <ReloadButton />
+          <FilterButton
+            onSelectType={setSelectedType}
+            selectedType={selectedType}
+          />
+
           <PageToggle />
         </div>
         <div className="flex flex-wrap flex-row justify-between gap-y-16 pt-16 pb-8 overflow-y-auto pr-4">
